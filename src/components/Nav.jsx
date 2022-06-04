fa7609dbdfb5a6c2549876cb00b5d18fb01a3a6a
Sohan <sohanshetty2001@gmail.com>
2022-06-04 17:24:33 +0530

    initial file setup :+1:
    darkmode toggle :-1:

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import usrProfile from '../assets/images/profile-1.jpg';
 import { SearchBar, ThemeToggler } from './all';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Nav = () => {
   return (
@@ -17,9 +17,9 @@ export const Nav = () => {
           <label className='btn btn-primary create-post-btn' for='create-post'>
             Create
           </label>
-          <div className='profile-photo'>
+          <Link to='/profile' className='profile-photo'>
             <img src={usrProfile} alt='profile picture' />
-          </div>
+          </Link>
         </div>
       </div>
     </nav>
--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggle } from '../features/theme/themeSlice';
 
 export const ThemeToggler = () => {
+  const theme = useSelector((state) => state.theme.mode);
+  const dispatch = useDispatch();
+
+  const toggleHandler = (evt) => {
+    dispatch(toggle());
+    document.body.classList = [theme];
+  };
+
   return (
-    <label for='toggler-checkbox' class='toggler'>
-      <input type='checkbox' id='toggler-checkbox' />
-      <span class='ball'></span>
-      {/* <i class='uil uil-sun sun'></i>
-        <i class='uil uil-moon moon'></i> */}
+    <label htmlFor='toggler-checkbox' className='toggler'>
+      <input type='checkbox' id='toggler-checkbox' onClick={toggleHandler} />
+      <span className='ball'></span>
       <FiSun className='sun' />
       <FiMoon className='moon' />
     </label>
